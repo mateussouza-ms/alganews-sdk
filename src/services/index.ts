cf0af94af0bd64cc67a13b9f60ddb7c39a26f37f
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-10-26 23:32:53 -0300

    Index PaymentService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { FileService } from "./FileService";
 export { MetricService } from "./MetricService";
+export { PaymentService } from "./PaymentService";
 export { PostService } from "./PostService";
 export { UserService } from "./UserService";
